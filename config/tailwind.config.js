eea7e37141c385ae9cc8afb6de48543891128c19
rakhatyusupov <seminar.p2p@gmail.com>
2026-02-02 23:22:02 +0300

    tailwind fix

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
   ],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Graphik LCG', 'system-ui', 'sans-serif'],
-        'mono': ['Reddit Mono', 'monospace'],
-        'grid': ['Gridlite PE', 'monospace'],
+        sans: ["Graphik LCG", "system-ui", "sans-serif"],
+        mono: ["Reddit Mono", "monospace"],
+        grid: ["Gridlite PE", "monospace"],
       },
     },
   },
   plugins: [],
-}
+};
